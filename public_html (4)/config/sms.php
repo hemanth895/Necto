855c4fa764f5fb84a18b41dc4b8c4e39ec1e13be
hemanth895 <hemanthreddy1331@gmail.com>
2026-02-02 23:41:08 +0530

    uopdated git ignore

--- public_html (4)/config/sms.php
+++
@@ -1,2 +0,0 @@
-<?php
-define('TWO_FACTOR_API_KEY', '6fe48363-df9f-11f0-a6b2-0200cd936042');
--- public_html (4)/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', function (event) {
-    const data = event.data.json();
-
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: '/assets/icon-192.png',
-        badge: '/assets/icon-192.png'
-    });
-});
